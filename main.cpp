ff8d7403da3927fbf9ae5989a63de3bf42bd8745
Takeda-senpai <84650617+HuskyDG@users.noreply.github.com>
2022-06-24 19:43:15 +0700

    revert

--- main.cpp
+++ main.cpp
@@ -8,107 +8,7 @@
 using namespace std;
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-
-#include <_system_properties.h>
-
-#include <system_properties/prop_area.h>
-#include <system_properties/system_properties.h>
-
-
-static SystemProperties system_properties;
-static_assert(__is_trivially_constructible(SystemProperties),
-              "System Properties must be trivially constructable");
-
-// This is public because it was exposed in the NDK. As of 2017-01, ~60 apps reference this symbol.
-// It is set to nullptr and never modified.
-__BIONIC_WEAK_VARIABLE_FOR_NATIVE_BRIDGE
-prop_area* __system_property_area__ = nullptr;
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-int __system_properties_init() {
-  return system_properties.Init(PROP_FILENAME) ? 0 : -1;
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-int __system_property_set_filename(const char*) {
-  return -1;
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-int __system_property_area_init() {
-  bool fsetxattr_failed = false;
-  return system_properties.AreaInit(PROP_FILENAME, &fsetxattr_failed) && !fsetxattr_failed ? 0 : -1;
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-uint32_t __system_property_area_serial() {
-  return system_properties.AreaSerial();
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-const prop_info* __system_property_find(const char* name) {
-  return system_properties.Find(name);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-int __system_property_read(const prop_info* pi, char* name, char* value) {
-  return system_properties.Read(pi, name, value);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-void __system_property_read_callback(const prop_info* pi,
-                                     void (*callback)(void* cookie, const char* name,
-                                                      const char* value, uint32_t serial),
-                                     void* cookie) {
-  return system_properties.ReadCallback(pi, callback, cookie);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-int __system_property_get(const char* name, char* value) {
-  return system_properties.Get(name, value);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-int __system_property_update(prop_info* pi, const char* value, unsigned int len) {
-  return system_properties.Update(pi, value, len);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-int __system_property_add(const char* name, unsigned int namelen, const char* value,
-                          unsigned int valuelen) {
-  return system_properties.Add(name, namelen, value, valuelen);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-int __system_property_delete(const char *name, bool trim_node) {
-  return system_properties.Delete(name, trim_node);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-uint32_t __system_property_serial(const prop_info* pi) {
-  return system_properties.Serial(pi);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-uint32_t __system_property_wait_any(uint32_t old_serial) {
-  return system_properties.WaitAny(old_serial);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-bool __system_property_wait(const prop_info* pi, uint32_t old_serial, uint32_t* new_serial_ptr,
-                            const timespec* relative_timeout) {
-  return system_properties.Wait(pi, old_serial, new_serial_ptr, relative_timeout);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-const prop_info* __system_property_find_nth(unsigned n) {
-  return system_properties.FindNth(n);
-}
-
-__BIONIC_WEAK_FOR_NATIVE_BRIDGE
-int __system_property_foreach(void (*propfn)(const prop_info* pi, void* cookie), void* cookie) {
-  return system_properties.Foreach(propfn, cookie);
-}
+#include <sys/_system_properties.h>
 
 std::string getprop(char const prop[]){
     char value[255];
@@ -177,7 +77,6 @@ void reset_sensitive_props(){
     auto hwcountry = getprop("ro.boot.hwcountry");
     if (!hwcountry.empty() && str_contains(hwcountry, "China"))
         setprop("ro.boot.hwcountry", "GLOBAL", false);
-    __system_property_delete("ro.build.selinux");
 
 }
 
@@ -198,20 +97,17 @@ Usage: %s NAME VALUE\n\
    return 0;
    }
 
-   const auto is_delete = strcmp(argv[1], "--delete") == 0;
-
+   
    const auto is_sensitive_props = strcmp(argv[1], "--sensitive-props") == 0;
 
    if (is_sensitive_props) {
         printf("Reset sensitive props\n");
         reset_sensitive_props();
+        return 0;
     }
-    else if (is_delete){
-        if (argc==3) __system_property_delete(argv[2]);
-        else return 1;
-    }
-    else
-        setprop(argv[1],argv[2], true);
+   
+
+    setprop(argv[1],argv[2], true);
 
     return 0;
 }
